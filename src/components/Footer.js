9e0bb1737fe24addf418a9c6fd5fb3f891afb93f
Doh Kim <dohyeongkim001024@gmail.com>
2023-11-07 17:11:19 -0700

    tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Source Code <a target="_blank" href="https://github.com/Doh1024/gym_app">Here</a></Typography>
   </Box>
 );
 
